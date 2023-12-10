c2a9eab6c2f167a242706152ea6aba9bd486dae0
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-10 22:10:15 +0800

    HeroBanner component

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,44 +1,35 @@
 import React from "react";
 import Image from "next/image";
-import { Raleway } from "next/font/google";
-
-const raleway = Raleway({
-  weight: "variable",
-  subsets: ["latin"],
-});
+import { raleway } from "./RalewayFont";
+import H1 from "./text/H1";
+import SectionWrapper from "./SectionWrapper";
+import ButtonLink from "./buttons/ButtonLink";
 
 const HeroBanner = () => {
   return (
-    <div className="flex flex-col items-center">
-      <span className="text-[200px]">🧝‍♀️</span>
-      <div className="text-center w-full">
-        <h1
-          className={`${raleway.className} font-light text-5xl w-full text-gray-900 sm:text-6xl`}
-        >
-          Data to enrich your online business
-        </h1>
-        <p
-          className={`${raleway.className} font-normal mt-6 text-lg leading-8 text-gray-600`}
-        >
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
-        </p>
-        <div
-          className={` ${raleway.className} mt-10 flex items-center justify-center gap-x-6`}
-        >
-          <a
-            href="#"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <SectionWrapper>
+      <div className="flex flex-col items-center">
+        <span className="text-[200px]">🧝‍♀️</span>
+        <div className="text-center w-full">
+          <H1>Data to enrich your online business</H1>
+          <p
+            className={`${raleway.className} font-normal mt-6 text-lg leading-8 text-gray-600`}
           >
-            Get started
-          </a>
-          <a href="#" className="text-sm font-medium leading-6 text-gray-900">
-            Learn more <span aria-hidden="true">→</span>
-          </a>
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          <div
+            className={` ${raleway.className} mt-10 flex items-center justify-center gap-x-6`}
+          >
+            <ButtonLink>Get started</ButtonLink>
+            <a href="#" className="text-sm font-medium leading-6 text-gray-900">
+              Learn more <span aria-hidden="true">→</span>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </SectionWrapper>
   );
 };
 
