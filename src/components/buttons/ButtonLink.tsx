590de8d6dbafd3821fb2da358e7347e5aed25f19
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-10 22:08:26 +0800

    ButtonLink component

---
+++ src/components/buttons/ButtonLink.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { raleway } from "../RalewayFont";
+
+interface ButtonProps extends React.HTMLAttributes<HTMLAnchorElement> {
+  children: React.ReactNode;
+}
+
+const ButtonLink = ({ children, ...attributes }: ButtonProps) => {
+  return (
+    <a
+      {...attributes}
+      className={`
+      ${raleway.className}
+      border-[1px]
+      border-slate-800
+      px-6 py-4 
+      text-sm 
+      font-medium 
+      text-slate-800
+      shadow-sm 
+      hover:bg-white/30
+      focus-visible:outline focus-visible:outline-2 
+      focus-visible:outline-offset-2 
+      focus-visible:outline-green-100
+      cursor-pointer`}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default ButtonLink;
