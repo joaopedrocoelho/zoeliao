9dabfd854d986003ce3f6352a58a6f8c6ed1d364
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-10 22:10:27 +0800

    Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import SectionWrapper from "./SectionWrapper";
+import H1 from "./text/H1";
+import MansoryGrid from "./projects/MansoryGrid";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <SectionWrapper>
+      <H1>Projects</H1>
+      <div className="pt-12">
+        <MansoryGrid />
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Projects;
