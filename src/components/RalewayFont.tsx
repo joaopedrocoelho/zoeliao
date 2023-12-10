55e571a3c28ede6b3c26a844e57e18c875654159
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-10 22:08:42 +0800

    Raleway font component

---
+++ src/components/RalewayFont.tsx
@@ -0,0 +1,12 @@
+import { Raleway } from "next/font/google";
+
+export const raleway = Raleway({
+  weight: "variable",
+  subsets: ["latin"],
+});
+
+export const ralewayItalic = Raleway({
+  weight: "variable",
+  subsets: ["latin"],
+  style: "italic",
+});
