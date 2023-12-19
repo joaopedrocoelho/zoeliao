f185d2b46ac8cde4db00337429f7f2cde82eeacb
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-19 19:27:12 +0800

    Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import P from "./text/P";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col px-4 py-8 border-t-[1px] border-slate-200">
+      <P>© 2023 Zoeliao</P>
+    </footer>
+  );
+};
+
+export default Footer;
