6bbb74ada02821135353121bef2f59e77a4eda0c
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-26 20:39:30 +0800

    Updated Services text

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -4,13 +4,13 @@ import { PiCaretDownThin } from "react-icons/pi";
 import { raleway } from "./RalewayFont";
 
 interface AccordionProps {
-  details: string;
+  children: React.ReactNode;
   summary: string;
   open: boolean;
   onClick: () => void;
 }
 
-const Accordion = ({ details, summary, open, onClick }: AccordionProps) => {
+const Accordion = ({ children, summary, open, onClick }: AccordionProps) => {
   return (
     <details
       open={true}
@@ -47,23 +47,38 @@ const Accordion = ({ details, summary, open, onClick }: AccordionProps) => {
       <div
         style={{
           transition: "max-height 0.5s ease",
-          maxHeight: open ? "300px" : "0px",
+          maxHeight: open ? "1000px" : "0px",
           overflow: "hidden",
         }}
       >
-        <p
-          className="px-6 py-8 font-light text-lg leading-8"
-          style={{
-            transition: "opacity 0.6s ease-in-out",
-            transitionDelay: "0.5s",
-            opacity: open ? 1 : 0,
-          }}
-        >
-          {details}
-        </p>
+        {children}
       </div>
     </details>
   );
 };
 
-export default Accordion;
+export const DetailsParagraph = ({
+  children,
+  open,
+}: {
+  children: React.ReactNode;
+  open: boolean;
+}) => {
+  return (
+    <div
+      className="px-6 py-8 font-light text-lg leading-8"
+      style={{
+        transition: "opacity 0.6s ease-in-out",
+        transitionDelay: "0.2s",
+        opacity: open ? 1 : 0,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const AccordionComponent = {
+  Accordion,
+  DetailsParagraph,
+};
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import H1 from "./text/H1";
-import Accordion from "./Accordion";
+import { AccordionComponent } from "./Accordion";
 import SectionWrapper from "./SectionWrapper";
 
 const Services = () => {
@@ -32,53 +32,114 @@ const Services = () => {
 
         <ul className="pt-12 lg:order-first lg:mx-auto">
           <li>
-            <Accordion
+            <AccordionComponent.Accordion
               summary="
             Social Media Management
           "
-              details="Our Social Media Management Service is the day-to-day running of your social media accounts, where we implement your custom social media strategy and aesthetic all in one place. We specialize in Instagram, TikTok, Pinterest, and LinkedIn."
               open={openIdx === 0}
               onClick={() => {
                 openIdx === 0 ? setOpenIdx(Infinity) : setOpenIdx(0);
               }}
-            />
+            >
+              <AccordionComponent.DetailsParagraph open={openIdx === 0}>
+                <div className="flex flex-col gap-y-4">
+                  <p>{`Unlock the full potential of your brand's social    
+        media presence with our tailored Social Media Management services. We specialize 
+        in crafting engaging strategies and aesthetics for Instagram, Pinterest and Facebook. 
+        Our meticulously designed packages encompass a wide range of essential services to supercharge your digital presence:
+            `}</p>
+                  <ul className="list-disc list-inside">
+                    <li>Social Media Audit & Strategy</li>
+                    <li>Monthly Content Planning & Scheduling</li>
+                    <li>Feed Planning</li>
+                    <li>Graphic Design</li>
+                    <li>Caption & Hashtags</li>
+                    <li>Keyword Research/Optimization</li>
+                    <li>New Product Launch Plan</li>
+                    <li>Stories Creation</li>
+                    <li>Monthly In-Depth Analytics</li>
+                  </ul>
+                </div>
+              </AccordionComponent.DetailsParagraph>
+            </AccordionComponent.Accordion>
           </li>
           <li>
-            <Accordion
+            <AccordionComponent.Accordion
               summary="
-            Social Media Management
+            Pinterest Management
           "
-              details="Our Social Media Management Service is the day-to-day running of your social media accounts, where we implement your custom social media strategy and aesthetic all in one place. We specialize in Instagram, TikTok, Pinterest, and LinkedIn."
               open={openIdx === 1}
               onClick={() => {
                 openIdx === 1 ? setOpenIdx(Infinity) : setOpenIdx(1);
               }}
-            />
+            >
+              <AccordionComponent.DetailsParagraph open={openIdx === 1}>
+                <div className="flex flex-col gap-y-4">
+                  <p>{`Ever wondered why Pinterest dances to its own beat? 
+                  It's because Pinterest isn't just a social media platform – 
+                  it's a search engine for inspiration! Welcome to our Pinterest Management, 
+                  where we offer a delightful 'hands-off' experience that lets you sit back and enjoy the magic!
+            `}</p>
+                  <ul className="list-disc list-inside">
+                    <li>Pinterest Audit & Strategy</li>
+                    <li>Template Curation</li>
+                    <li>New Account Setup & Optimization</li>
+                    <li>SEO-Powered Profile</li>
+                    <li>Niche-Specific Boards</li>
+                    <li>Fresh Pins Every Month</li>
+                    <li>Optimized Titles & Descriptions</li>
+                    <li>Monthly In-Depth Analytics</li>
+                  </ul>
+                </div>
+              </AccordionComponent.DetailsParagraph>
+            </AccordionComponent.Accordion>
           </li>
-
           <li>
-            <Accordion
+            <AccordionComponent.Accordion
               summary="
-            Social Media Management
+            Blog Writer
           "
-              details="Our Social Media Management Service is the day-to-day running of your social media accounts, where we implement your custom social media strategy and aesthetic all in one place. We specialize in Instagram, TikTok, Pinterest, and LinkedIn."
               open={openIdx === 2}
               onClick={() => {
                 openIdx === 2 ? setOpenIdx(Infinity) : setOpenIdx(2);
               }}
-            />
+            >
+              <AccordionComponent.DetailsParagraph open={openIdx === 2}>
+                <div className="flex flex-col gap-y-4">
+                  <p>{`Unlock the SEO-boosting power of your brand with our 
+                  customized Blog Writing services. 
+                  We excel at creating engaging narratives that resonate with your audience.
+                   From thought-provoking articles to informative pieces, our tailored blogs help with SEO by enhancing your online presence and driving engagement.
+            `}</p>
+                </div>
+              </AccordionComponent.DetailsParagraph>
+            </AccordionComponent.Accordion>
           </li>
           <li>
-            <Accordion
+            <AccordionComponent.Accordion
               summary="
-            Social Media Management
+            Content Creation
           "
-              details="Our Social Media Management Service is the day-to-day running of your social media accounts, where we implement your custom social media strategy and aesthetic all in one place. We specialize in Instagram, TikTok, Pinterest, and LinkedIn."
               open={openIdx === 3}
               onClick={() => {
                 openIdx === 3 ? setOpenIdx(Infinity) : setOpenIdx(3);
               }}
-            />
+            >
+              <AccordionComponent.DetailsParagraph open={openIdx === 3}>
+                <div className="flex flex-col gap-y-4">
+                  <p>{`
+                  Is your business craving captivating content but short on resources? 
+                  Look no further – we're here to bring your vision to life! 
+                  Introducing our Content Creation service, your one-stop solution for all your content needs. 
+                  Whether you're seeking a partner to film exceptional footage or craft compelling visuals,
+                   we've got you covered. Seamlessly integrated with our Social Media Management packages,
+                    or available as a standalone service, our Content Creation service ensures you're always 
+                    armed with the perfect content for your projects.
+
+            `}</p>
+                </div>
+              </AccordionComponent.DetailsParagraph>
+            </AccordionComponent.Accordion>
           </li>
         </ul>
       </div>
