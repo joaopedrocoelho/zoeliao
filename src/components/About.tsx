9f0692dc420803cec878529d133a1e4eaae7d175
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-12-26 20:42:19 +0800

    Updated about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -35,22 +35,34 @@ const About = () => {
           pb-3
           italic`}
           >
-            Hi! My name is Zoe! I&apos;m a social media manager based in Taiwan.
+            {
+              "Hey there, I'm Zoe, your go-to Social Media Management Specialist."
+            }
           </p>
           <P>
-            I am a software engineer with a passion for building beautiful and
-            functional websites. I am a recent graduate of the University of
-            California, Berkeley with a Bachelors of Arts in Computer Science.
+            Throughout my career in the creative field, I have had the
+            opportunity to work across various products and industries, gaining
+            valuable experience in market research, branding, digital marketing,
+            blog writing and more.
           </P>
           <P>
-            I have experience working with React, Next.js, TypeScript,
-            JavaScript, HTML, CSS, and Python. I am currently looking for
-            full-time opportunities in software engineering.
+            {`One of the cool things about my journey is that I've had the privilege of working in both 
+          Asian and Western markets for over 5 years, whether it's with big-name companies or pursuing my own projects.`}
           </P>
           <P>
-            I am also a freelance social media manager and content creator. I
-            specialize in Instagram, TikTok, Pinterest, and LinkedIn. I have
-            experience working with Canva, Adobe Photoshop, and Adobe Lightroom.
+            I am a native Mandarin speaker, and English is my second language,
+            in which I am fluent. Additionally, I am currently in the process of
+            learning Portuguese, which will be my third language. My linguistic
+            abilities span a diverse spectrum, enabling me to effectively
+            communicate and connect with individuals from various backgrounds
+            and cultures.
+          </P>
+          <P>
+            {`
+            So, if you're on the lookout for someone to boost your brand, 
+            fit right into your team, or simply share some exciting ideas, let's connect! 
+            I'm all about making meaningful connections and creating magic together!
+            `}
           </P>
         </div>
       </div>
